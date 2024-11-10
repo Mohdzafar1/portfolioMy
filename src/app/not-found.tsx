7e694d188da5b0820197e05f671fb733ae7880b7
Mohd Zafar <mohdzafar97597@gmail.com>
2024-11-10 20:25:20 +0530

    create custom not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound(){
+    return(
+    <>
+    <h1>Page not found</h1>
+    <p>Cound not found this page</p>
+    </>
+    )
+}
--- src/app/product/[productId]/review/[productReview]/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function ReviewDetails(){
-    return <h1>Review details</h1>
-}
